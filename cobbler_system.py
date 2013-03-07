7062b2147031452b7cb07f366fd554dbbf0fb249
Stephen Benjamin <stbenjam@redhat.com>
2013-03-07 18:35:25 +0100

    Initial commit

---
+++ cobbler_system.py
@@ -0,0 +1,82 @@
+"""
+CobblerSystem Class
+
+Does the actual interaction with Cobbler.
+
+"""
+
+from config import Config
+import xmlrpclib
+
+class CobblerSystem:
+
+    def __init__(self):
+        config = Config()
+        self._cobbler = xmlrpclib.Server(config.api_url)
+        self._token   = self._cobbler.login(config.username, config.password)
+        self._system = self._cobbler.new_system(self._token)
+
+    def _modify(self, key, value):
+        self._cobbler.modify_system(self._system,key,value,self._token)
+
+    def save(self):
+        self._cobbler.save_system(self._system,self._token)
+    
+    def sync(self):
+        self._cobbler.sync(self._token)
+
+    def commit(self):
+        self.save()
+        self.sync()
+
+    def set_hostname(self, hostname):
+        """
+        Sets the cobbler system hostname and name
+        """
+        self._modify("name", hostname)
+        self._modify("hostname", hostname)
+
+
+    def set_profile(self, profile):
+        """
+        Sets the cobbler system profile name
+        """
+        self._modify("profile", profile)
+
+    def set_kernel_opts(self, **kwargs):
+        """
+        Sets kernel options.  Pass them as named keywords.  E.g., set_kernel_ops(self, foo=bar)
+        For kernel opts without values, set it to None.  e.g. set_kernel_opts(self, quiet=None)
+        """
+        self._modify("kernel_options", kwargs)
+
+
+    def set_ks_meta(self, **kwargs):
+        """
+        Sets kickstart metadata   Pass as named keywords.  E.g., set_ks_meta(self, foo=bar)
+        """
+        self._modify("ks_meta", kwargs)
+
+
+    def create_interface(self, interface="eth0", mac="", ip="", subnet="", gateway="", static=False):
+        """
+        Creates a simple network interface
+        """
+        interface = {}
+        if mac:
+            interface["macaddress-%s" % interface] = mac
+        if ip:
+            interface["ipaddress-%s" % interface] = ip
+        if subnet:
+            interface["subnet-%s" % interface] = subnet
+        if gateway:
+            self._modify("gateway", gateway)
+
+        interface["static-%s" % interface] = static
+
+        self._modify("modify_interface", interface)
+
+
+    def set_netboot(netboot):
+        self._modify("netboot_enabled", netboot)
+
---
+++ config.py
@@ -0,0 +1,70 @@
+"""
+config.py has the configuration for the cobbler-puppet integration tool.
+
+Copyright 2012, Stephen Benjamin
+Stephen Benjamin <skbenja@gmail.com>
+
+"""
+
+import os
+import sys
+import ConfigParser
+
+class Config:
+
+    def __init__(self, configFile="../etc/cobbler-puppet.conf"):
+        self._cp = ConfigParser.ConfigParser()
+        self._cp.readfp(open(configFile))
+
+        self._config = { }
+
+        # Get cobbler configuration
+        if self._cp.getboolean("cobbler", "use_rhn_auth"):
+            self.get_rhn_credentials()
+        else:
+            self._config['username'] = self._cp.get("cobbler", "username")
+            self._config['password'] = self._cp.get("cobbler", "password")
+            self._config['api_url'] = self._cp.get("cobbler", "api_url")
+
+        # Get Puppet ENC Configuration
+        self._config['node_lister'] = self._cp.get("puppet", "node_lister")
+        self._config['external_nodes'] = self._cp.get("puppet", "external_nodes")
+
+    def get_rhn_credentials(self):
+        """
+        If we're using the rhn authorization module, we get the taskomatic user's
+        password by importing the spacewalk modules
+        """
+        sys.path.append('/usr/share/rhn')
+        try:
+            from spacewalk.common.rhnConfig import initCFG, CFG
+        except ImportError:
+            raise ConfigError("This is not a Spacewalk server, but the configuration says I am.")
+
+        initCFG()
+        self.config['username'] = 'taskomatic_user'
+        self.config['password'] = CFG.SESSION_SECRET_1
+ 
+    def __getattr__(self, name):
+        return self._config[name]
+
+
+class ConfigError(Exception):
+    """
+    Exception when there's a problem with the configuration.
+    """ 
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
+if __name__ == "__main__":
+    config = Config("../etc/cobbler-puppet.conf")
+    print config.username
+    print config.password
+    print config.api_url
+    print config.external_nodes
+    print config.node_lister
+
