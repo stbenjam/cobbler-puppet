82db4b3c3306c52acd5fdd79b909f0146843c8f1
Stephen Benjamin <stbenjam@redhat.com>
2013-03-13 16:41:17 +0100

    Removed errors.py, now in custom_exceptions.py

--- src/cobbler_puppet/errors.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-class InvalidSystemDefinition(Exception):
-    def __init__(self, value):
-        self.value = value
-    def __str__(self):
-        return repr(self.value)
-
